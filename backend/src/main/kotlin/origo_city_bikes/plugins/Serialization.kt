23124d40cb8994a946949c36cf36ae24484f33ad
Sondre Ohlsson <81510347+sn-ohlsson@users.noreply.github.com>
2024-05-16 00:11:37 +0200

    Feat: Start of proper requests (#5)

--- backend/src/main/kotlin/origo_city_bikes/plugins/Serialization.kt
+++ backend/src/main/kotlin/origo_city_bikes/plugins/Serialization.kt
@@ -1,5 +1,9 @@
 package origo_city_bikes.plugins
 
+import io.ktor.client.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
 import io.ktor.serialization.gson.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.contentnegotiation.*
@@ -14,6 +18,19 @@ fun Application.configureSerialization() {
 
 	routing {
 		get("/origo") {
+
+			val client = HttpClient()
+			// TODO: Consider client.use due to single request being made
+			// TODO: Replace url
+			// TODO: Separate paths for separate requests/endpoints
+
+			val response: HttpResponse = client.request("https://gbfs.urbansharing.com/oslobysykkel.no/gbfs.json") {
+				method = HttpMethod.Get
+				headers {
+					append("Client-Identifier", "origo-jobinterview-sn-ohlsson")
+				}
+			}
+
 			call.respondText("Text from backend here")
 		}
 	}
