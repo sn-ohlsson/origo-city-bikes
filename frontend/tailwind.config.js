d8e68f5251f9b3bbc4ac092d9c8b2980fe9d1c31
Sondre Ohlsson <81510347+sn-ohlsson@users.noreply.github.com>
2024-05-16 11:01:18 +0200

    Style: Some basic styling (fonts, logo). Add Tailwind config. (#6)

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+    content: ["src/**/*.elm"],
+    darkMode: "media", // or 'media' or 'class'
+    theme: {
+        fontFamily: {
+            'display': ['Oslo Sans', 'Arial'],
+            'body': ['Oslo Sans', 'Arial'],
+            'sans': ['Oslo Sans', 'Arial'],
+            'mono': ['JetBrains Mono', 'monospace']
+        },
+        opacity: {},
+        extend: {
+            dropShadow: {
+                'dark': '0 0px 10px rgba(255, 255, 255, 0.3)'
+            },
+            animation: {
+                'fadeIn': 'fade 750ms ease-out forwards',
+                'fadeInBody': 'fade 750ms 400ms ease-out forwards'
+            },
+            keyframes: {
+                fade: {
+                    'from': {opacity: '0', transform: 'translateY(1rem)'},
+                    'to': {opacity: '1', transform: 'translateY(0)'}
+                },
+            }
+        }
+    },
+    variants: [],
+    plugins: [],
+}
